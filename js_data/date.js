ff28923e274b683700f139959ece893c80ff75d6
Deepak <101124202+deepaksamal23@users.noreply.github.com>
2023-09-27 07:05:50 +0000

    date& time

---
+++ js_data/date.js
@@ -0,0 +1,13 @@
+const myDate=new Date()
+const data =myDate.toString();
+// console.log(data);
+ const odi= myDate.toDateString()
+// console.log(odi);
+const od1i= myDate.toLocaleTimeString()
+// console.log(od1i);
+
+const mycreateDate=new Date(2021,1,12)
+console.log(mycreateDate.toDateString());
+console.log(mycreateDate.toLocaleDateString());
+console.log(myDate.getMonth()+1);
+console.log(myDate.getFullYear());
